1d057f18ad5468bfc080916892839ddeff75e53e
Michael G <mcg1441@uncw.edu>
2023-11-24 16:16:38 -0500

    Removed test class and driver

--- records/Daily.java
+++
@@ -1,9 +0,0 @@
-package records;
-
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-
-public class Daily implements Serializable {
-    public String records;
-}
