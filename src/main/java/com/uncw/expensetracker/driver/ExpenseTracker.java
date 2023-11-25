047cdf96518652028dae79adf203f26b7fbab082
Michael G <mcg1441@uncw.edu>
2023-11-25 18:38:57 -0500

    Fixed null reference

--- src/main/java/com/uncw/expensetracker/driver/ExpenseTracker.java
+++ src/main/java/com/uncw/expensetracker/driver/ExpenseTracker.java
@@ -5,11 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
+
 //driver
 public class ExpenseTracker extends Application{
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/LoginPage.fxml")));
 
         Scene scene = new Scene(root);
         stage.setTitle("ExpenseTracker");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module com.uncw.expensetracker {
     requires javafx.fxml;
     requires com.google.gson;
 
-    opens com.uncw.expensetracker to javafx.fxml;
     exports com.uncw.expensetracker.records;
     exports com.uncw.expensetracker.driver;
-    opens com.uncw.expensetracker.driver to javafx.fxml;
 }
